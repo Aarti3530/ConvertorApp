c68ad8dc699eb5dc26eb0efad978263cf162bf75
aarti <aarti6264744312@gmail.com>
2021-06-07 18:33:55 +0530

    New changes

--- app/src/main/java/com/example/convert/MainScreen.java
+++ app/src/main/java/com/example/convert/MainScreen.java
@@ -7,22 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.convert.ASCII;
-import com.example.convert.Area;
-import com.example.convert.Calculator;
-import com.example.convert.Data;
-import com.example.convert.Length;
-import com.example.convert.Mass;
-import com.example.convert.Money;
-import com.example.convert.R;
-import com.example.convert.Speed;
-import com.example.convert.Temperature;
-import com.example.convert.Time;
-import com.example.convert.Volume;
-import com.example.convert.Weigth;
-
 public class MainScreen extends AppCompatActivity {
-    Button area,ascii,calculator,dataSize,length,mass,money,speed,temperature,time,volume,weight;
+    Button ascii,calculator,dataSize,money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +23,12 @@ public class MainScreen extends AppCompatActivity {
     }
 
     private void bndView() {
-        area =  findViewById(R.id.area);
         ascii =  findViewById(R.id.ascii);
         calculator = findViewById(R.id.calsi);
         dataSize =  findViewById(R.id.datasize);
-        length =  findViewById(R.id.length);
-        mass =  findViewById(R.id.mass);
         money =  findViewById(R.id.money);
-        speed =  findViewById(R.id.speed);
-        temperature =  findViewById(R.id.temperature);
-        time = findViewById(R.id.time);
-        volume =  findViewById(R.id.volume);
-        weight =  findViewById(R.id.weight);
     }
     private void set(){
-        area.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Area.class);
-                startActivity(intent);
-            }
-        });
         ascii.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -83,22 +53,7 @@ public class MainScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        length.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Length.class);
-                startActivity(intent);
-            }
-        });
-        mass.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Mass.class);
-                startActivity(intent);
-            }
-        });
         money.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -107,45 +62,6 @@ public class MainScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        speed.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Speed.class);
-                startActivity(intent);
-            }
-        });
-        temperature.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Temperature.class);
-                startActivity(intent);
-            }
-        });
-        time.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Time.class);
-                startActivity(intent);
-            }
-        });
-        volume.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Volume.class);
-                startActivity(intent);
-            }
-        });
-        weight.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainScreen.this, Weigth.class);
-                startActivity(intent);
-            }
-        });
     }
 }
