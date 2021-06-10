9acf6a63ab84f2706ebbcbeab50190cc9df46cc9
aarti <aarti6264744312@gmail.com>
2021-06-10 17:41:59 +0530

    inor changes

--- app/src/main/java/com/example/convert/ASCII.java
+++ app/src/main/java/com/example/convert/ASCII.java
@@ -3,12 +3,63 @@ package com.example.convert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ASCII extends AppCompatActivity {
-
+    Button display,clear;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a_s_c_i_i);
+        bindView();
+        clickListener();
+    }
+
+    private void bindView() {
+        display = findViewById(R.id.Enter);
+        clear = findViewById(R.id.Clear);
+        text = findViewById(R.id.ascii);
+    }
+
+    private void clickListener() {
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = String.valueOf(text.getText());
+                char ch = s.charAt(0);
+                if(Character.isDigit(ch)){
+                    int N = Integer.parseInt(s);
+                      digitAscii(N);
+                }
+                else if(Character.isLetter(ch)){
+                    int ascii = (int) ch;
+                    text.setText(""+ascii+"");
+                }
+                else
+                    text.setText("Invalid Input");
+            }
+
+            private void digitAscii(int ch) {
+                if(ch == 0){
+                    text.setText(""+48);
+                    return;
+                }
+                while(ch >= 0){
+                    int d = ch % 10;
+                    text.setText(d+48+"");
+                    ch = ch/10;
+                }
+            }
+
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(null);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/convert/Calculator.java
+++ app/src/main/java/com/example/convert/Calculator.java
@@ -3,12 +3,167 @@ package com.example.convert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.view.View.*;
 
 public class Calculator extends AppCompatActivity {
-
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,C,mul,add,div,sub,equal;
+    EditText look;
+    boolean Aadd,Asub,Amul,Adiv;
+    float num1,num2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        bindView();
+        clickListener();
+    }
+
+    private void clickListener() {
+        b1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               look.setText(look.getText()+"1");
+            }
+        });
+        b2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"2");
+            }
+        });
+        b3.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"3");
+            }
+        });
+        b4.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"4");
+            }
+        });
+        b5.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"5");
+            }
+        });
+        b6.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"6");
+            }
+        });
+        b7.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"7");
+            }
+        });
+        b8.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"8");
+            }
+        });
+        b9.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"9");
+            }
+        });
+        b0.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText(look.getText()+"0");
+            }
+        });
+        C.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                look.setText("");
+            }
+        });
+        add.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Float.parseFloat(look.getText()+"");
+                Aadd = true;
+                look.setText(null);
+            }
+        });
+        mul.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Float.parseFloat(look.getText()+"");
+                Amul = true;
+                look.setText(null);
+            }
+        });
+        div.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Float.parseFloat(look.getText()+"");
+                Adiv = true;
+                look.setText(null);
+            }
+        });
+        sub.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Float.parseFloat(look.getText()+"");
+                Asub = true;
+                look.setText(null);
+            }
+        });
+        equal.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num2 = Float.parseFloat(look.getText()+"");
+                look.setText(null);
+                if(Aadd == true){
+                    look.setText(num1 + num2 + "");
+                    Aadd = false;
+                }
+                else if(Asub == true){
+                    look.setText(num1 - num2 + "");
+                    Asub = false;
+                }
+                else if(Amul == true){
+                    look.setText(num1 * num2 + "");
+                    Amul = false;
+                }
+                else if(Adiv == true){
+                    look.setText(num1 / num2 + "");
+                    Adiv = false;
+                }
+            }
+        });
+    }
+
+    private void bindView() {
+        look = (EditText) findViewById(R.id.pannel);
+        b1 = findViewById(R.id.num1);
+        b2 = findViewById(R.id.num2);
+        b3 = findViewById(R.id.num3);
+        b4 = findViewById(R.id.num4);
+        b5 = findViewById(R.id.num5);
+        b6 = findViewById(R.id.num6);
+        b7 = findViewById(R.id.num7);
+        b8 = findViewById(R.id.num8);
+        b9 = findViewById(R.id.num9);
+        b0 = findViewById(R.id.num0);
+        C = findViewById(R.id.C);
+        equal = findViewById(R.id.equal);
+        mul = findViewById(R.id.mul);
+        sub = findViewById(R.id.sub);
+        div = findViewById(R.id.div);
+        add = findViewById(R.id.add);
     }
 }
